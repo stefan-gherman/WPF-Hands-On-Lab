2c32f618483c46fe2623dd9d34bc19d4b65fb524
szrudi <szrudi@gmail.com>
2020-06-28 03:23:19 +0200

    Initial commit

---
+++ WPF Hands-on exercise/Constants/ImageConstants.cs
@@ -0,0 +1,13 @@
+namespace HandsOnLab1.Constants
+{
+    /// <summary>
+    /// Constants for image resources.
+    /// </summary>
+    internal class ImageConstants
+    {
+        /// <summary>
+        /// The Save Icon
+        /// </summary>
+        public static readonly string SAVE_ICON = @"pack://application:,,,/HandsOnLab1;component/Images/Save.gif";
+    }
+}
