8970cdf664ac37153b71f42906b5ada22d4a3a96
Stefan Gherman <scgheman@gmail.com>
2020-08-18 05:15:23 +0300

    Add user edit controller

--- WPF Hands-on exercise/CustomerEdit.xaml.cs
+++ WPF Hands-on exercise/CustomerEdit.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HandsOnLab1.ClientEntities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,14 +15,58 @@ using System.Windows.Shapes;
 
 namespace HandsOnLab1
 {
-    /// <summary>
-    /// Interaction logic for CustomerEdit.xaml
-    /// </summary>
-    public partial class CustomerEdit : UserControl
+    public partial class CustomerEdit : UserControl,
+INotifyPropertyChanged
     {
-        public CustomerEdit()
+        private CustomerUpdate _customer = new
+        CustomerUpdate(123);
+public CustomerEdit()
         {
             InitializeComponent();
+            DataContext = this;
         }
+        public CustomerUpdate Customer
+        {
+            get { return _customer; }
+            set
+            {
+                if (_customer != value)
+                {
+                    _customer = value;
+                    OnPropertyChanged("Customer");
+                }
+            }
+        }
+        private void CanSave(object sender,
+        CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void ExecuteSave(object sender,
+        ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Assume the save worked.");
+        }
+        #region INotifyPropertyChanged Members
+        /// <summary>
+        /// Implicit implementation of the INotifyPropertyChanged.PropertyChanged event.
+        /// /// </summary>
+public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Throws the <c>PropertyChanged</c> event.
+        /// </summary>
+        /// <param name="propertyName">The name of theproperty that was modified.</param>
+protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new
+                PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        
+
+#endregion
     }
 }
